db5d9703a6c0f46d71f15677543b7dda5e15a5d0
Igor Barkowski <igor.barkowski@gmail.com>
2019-02-03 21:30:23 +0100

    Change lead background

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('scripts', function() {
             }
         }))
         .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('./js'));
 });
 
 gulp.task('styles', function () {
@@ -31,6 +31,6 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('js/scripts.js', gulp.series('scripts'));
-    gulp.watch('scss/styles.scss', gulp.series('styles'));
+    gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
 });
